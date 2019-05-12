e32387b28970fc422541ef8b7e24aaf4c55a1e12
Luv Karakoti <luvkarakoti@Luvs-MacBook-Air.local>
2019-05-12 09:38:45 +0100

    Refactor project to TypeScript

--- index.old.js
+++ index.old.js
@@ -6,36 +6,36 @@ const path = require('path');
 
 console.log("Picking up files from INPUT directory");
 
-const THIS_INPUT = __dirname+INPUT;
+const THIS_INPUT = __dirname + INPUT;
 const THIS_OUTPUT = path.join(__dirname, path.sep, '..', OUTPUT);//__dirname+OUTPUT;
 
-File.createDirectory(THIS_INPUT).then(status=>{
+File.createDirectory(THIS_INPUT).then(status => {
     return File.getFileNames(THIS_INPUT);
-}).then(files=>{
+}).then(files => {
     var classes = new Set();
-    files.forEach(file=>{
-        var jsonString = File.getFileData(THIS_INPUT+file);
+    files.forEach(file => {
+        var jsonString = File.getFileData(THIS_INPUT + file);
         var jsonElement = JSON.parse(jsonString);
         var queue = [{
-            "name":"Start",
-            "json":jsonElement
+            "name": "Start",
+            "json": jsonElement
         }];
-        while(queue.length){
+        while (queue.length) {
             var top = queue.shift();
             var keys = Object.keys(top['json']);
             var newClass = new ClassMaker.ClassMaker();
             newClass.setName(top['name']);
-            keys.forEach(key=>{
+            keys.forEach(key => {
                 value = top['json'][key];
-                if(value instanceof Array){
-                    queue.push({
-                        "name":key,
-                        "json":value[0]
-                    });
-                } else if(value instanceof Object){
+                if (value instanceof Array) {
                     queue.push({
                         "name": key,
-                        "json":value
+                        "json": value[0]
+                    });
+                } else if (value instanceof Object) {
+                    queue.push({
+                        "name": key,
+                        "json": value
                     });
                 }
                 newClass.setAttribute(key);
@@ -44,27 +44,27 @@ File.createDirectory(THIS_INPUT).then(status=>{
         }
     });
     return classes;
-}).then(classes=>{
+}).then(classes => {
     var classStrings = new Set();
     console.log(classes.size);
-    classes.forEach(function(className){
+    classes.forEach(function (className) {
         classStrings.add({
             "title": className.getName(),
-            "content":className.makeES6Class()
+            "content": className.makeES6Class()
         });
     });
     return classStrings;
-}).then(classStrings=>{
-    File.removeDirectory(THIS_OUTPUT).then(status=>{
-        File.createDirectory(THIS_OUTPUT).then(data=>{
-            classStrings.forEach((classString)=>{
-                if(classString!=="[object Object]"){
-                    var status = File.saveFile(THIS_OUTPUT+classString.title+'.js', classString.content);
+}).then(classStrings => {
+    File.removeDirectory(THIS_OUTPUT).then(status => {
+        File.createDirectory(THIS_OUTPUT).then(data => {
+            classStrings.forEach((classString) => {
+                if (classString !== "[object Object]") {
+                    var status = File.saveFile(THIS_OUTPUT + classString.title + '.js', classString.content);
                 }
             });
             console.log("DONE");
         });
     });
-}).catch(err=>{
+}).catch(err => {
     console.log(err);
 });
